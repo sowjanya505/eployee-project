541c184162abba6c79227b73a3570139b7c28e11
sowjanya505 <62980183+sowjanya505@users.noreply.github.com>
2020-04-03 13:25:21 +0530

    Update EmployeeServiceImpl.java

--- EmployeeServiceImpl.java
+++ EmployeeServiceImpl.java
@@ -1,40 +1,31 @@
-package com.ahmadsolehin.SUSAHCRUDNIE.service;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ahmadsolehin.SUSAHCRUDNIE.model.Employee;
 import com.ahmadsolehin.SUSAHCRUDNIE.repository.EmployeeRepository;
 
-@Service
-public class EmployeeServiceImpl implements EmployeeService {
-    @Autowired
-    private EmployeeRepository employeeRepository;
 
-    @Override
+public class EmployeeServiceImpl implements emp {
+  
+    private EmployeeRepository emp;
+  
     public Iterable<Employee> findAll() {
-        return employeeRepository.findAll();
+        return emp.findAll();
     }
 
-    @Override
     public List<Employee> search(String q) {
-        return employeeRepository.findByNameContaining(q);
+        return emp.findByNameContaining(q);
     }
 
-    @Override
     public Employee findOne(int id) {
-        return employeeRepository.findOne(id);
+        return emp.findOne(id);
     }
 
-    @Override
     public void save(Employee contact) {
-        employeeRepository.save(contact);
+        emp.save(contact);
     }
-
-    @Override
+ 
     public void delete(int id) {
-        employeeRepository.delete(id);
+        emp.delete(id);
     }
 }
